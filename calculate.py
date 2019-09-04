8e6117a3bd1b815d818900465819ede83f1c0a89
Sayter99 <ing5698yi@hotmail.com>
2019-09-04 17:12:37 -0600

    make it more flexible

--- calculate.py
+++ calculate.py
@@ -1,5 +1,27 @@
+import argparse
 import math_lib
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description = 'This program calculate add/div of given inputs' 
+    )
+    parser.add_argument('inputs',
+                        type=float,
+                        nargs='+',
+                        help='two input numbers'
+    )
+
+    args_parse = parser.parse_args()
+
+    return args_parse
+
 if __name__ == '__main__':
-    print('add(2.1, 3) = %f' % math_lib.add(2.1, 3))
-    print('div(300, 22) = %f' % math_lib.div(300, 22))
+    args = parse_args()
+    if (len(args.inputs) < 2):
+        print('must input 2 numbers')
+    else:
+        a = args.inputs[0]
+        b = args.inputs[1]
+        print('your input values are: %f and %f' % (a, b))
+        print('the sum is %f' % math_lib.add(a, b))
+        print('first/second is %f' % math_lib.div(a, b))
--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-python calculate.py
+#!/bin/bash
+
+echo "..running calculate.py.."
+python3 calculate.py 200 23
